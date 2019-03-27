c67df3767ae7df6cc2064bf07ee039cf3e62705a
Starry <starry90@qq.com>
2019-03-27 18:42:53 +0800

    Add adb.sh

---
+++ adb.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+set -u
+
+if [ $# -lt 1 ]
+then
+    echo "example: $0 应用包名"
+    exit
+fi
+
+app_package=$1
+
+#adb logcat -v threadtime | grep -F "`adb shell ps | grep 应用包名 | cut -c10-15`"
+function app_log(){
+    shell1="`adb shell ps | grep ${app_package} | cut -c10-15`"
+    process=${shell1}
+    result=""
+    for element in ${process}
+    do
+       echo "应用进程id：${element}"
+       result="${result}${element}|"
+    done
+    #去掉最后一个竖杠
+    result="${result%?}"
+    shell2="adb shell logcat -v threadtime | grep -E \"${result}\""
+    echo ${shell2}
+}
+
+function input(){
+    while true
+    do
+        echo "[1: logcat] [q: exit]"
+        echo -n "input: "
+        read number
+        if [[ ${number} = "1" ]]
+        then
+            app_log
+            break
+        elif [[ ${number} = "q" ]]
+        then
+            break
+        fi
+    done
+}
+
+input
