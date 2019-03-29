6d1e37ad5cf7f3787722b0d3a345dadb226faa2c
Starry <starry90@qq.com>
2019-03-29 14:55:38 +0800

    Add other command for adb.sh

--- adb.sh
+++ adb.sh
@@ -34,15 +34,58 @@ function app_log(){
     adb shell logcat -v threadtime | grep --colour=always -F "`adb shell ps | grep ${app_package} | awk '{print $2}'`"
 }
 
-function input(){
+function app_version(){
+    adb shell pm dump ${app_package} | grep --colour=always "version"
+}
+
+function app_path(){
+    adb shell pm path ${app_package} | awk -F ":" '{print $2}'
+}
+
+function app_top_activity(){
+    adb shell dumpsys activity top | head -n 5
+}
+
+function app_monkey(){
+    adb shell monkey -p ${app_package} -v 1000
+}
+
+function app_clear_data(){
+    adb shell pm clear ${app_package}
+}
+
+function adb_finish(){
+    echo "The End..."
+}
+function action(){
     while true
     do
-        echo "[1: logcat] [q: exit]"
+        echo "[1:应用日志信息] [2:应用版本信息]  [3:应用apk位置] [4:当前栈顶Activity] [5:压测1千次] [c:清除应用数据] [q:退出]"
         echo -n "input: "
         read number
         if [[ ${number} = "1" ]]
         then
             app_log
+            break
+        elif [[ ${number} = "2" ]]
+        then
+            app_version
+            break
+        elif [[ ${number} = "3" ]]
+        then
+            app_path
+            break
+        elif [[ ${number} = "4" ]]
+        then
+            app_top_activity
+            break
+        elif [[ ${number} = "5" ]]
+        then
+            app_monkey
+            break
+        elif [[ ${number} = "c" ]]
+        then
+            app_clear_data
             break
         elif [[ ${number} = "q" ]]
         then
@@ -51,4 +94,7 @@ function input(){
     done
 }
 
-input
+# 捕获退出信号
+trap adb_finish EXIT
+action
+exit
